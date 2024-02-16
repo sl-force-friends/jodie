ea80c15728c4a4556ea2da7f4b2b42e0ef956175
Gabriel CHUA <gabriel_chua@tech.gov.sg>
2024-02-16 09:29:16 +0800

    update requirements.txt

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 """
 app.py
 """
+# For hosting on streamlit community cloud
+try:
+    __import__('pysqlite3')
+    import sys
+    sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+except:
+    pass
+
 import streamlit as st
 
 import tab_add_jd
