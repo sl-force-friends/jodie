b75a80759adbd17619a6574d20ca98ca0747fc46
Gabriel CHUA <gabriel_chua@tech.gov.sg>
2024-02-16 09:27:22 +0800

    use st secrets

--- utils/config.py
+++ utils/config.py
@@ -3,11 +3,13 @@ config.py
 """
 import os
 
+import streamlit as st
+
 APP_TITLE = "🔎 JoDIE \n **:blue[Jo]b :blue[D]escription :blue[I]ntelligent :blue[E]nhancer**"
 TAB_NAMES = ["1️⃣ **Enter JD**", "2️⃣ **AI-Feedback**", "3️⃣ **Re-Written JD**"]
 
-API_KEY = os.getenv("JODIE_API_KEY")
-AZURE_ENDPOINT = os.getenv("JODIE_ENDPOINT", "MISSING")
+API_KEY = st.secrets["JODIE_API_KEY"]
+AZURE_ENDPOINT = st.secrets["JODIE_ENDPOINT"]
 API_VERSION = "2024-02-15-preview"
 
 SESSION_STATE_DEFAULT_NONE = ["mcf_url", "title_placeholder", "desc_placeholder", "user_title", "user_desc"]
